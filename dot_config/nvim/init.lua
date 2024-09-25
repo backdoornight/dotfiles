84c8535548db753a39b655ebf7764c5b7369a247
Roger Hughes <roger@roghu.com>
2024-09-25 17:42:28 -0400

    feat(neovim): add lazy plugin manager

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,3 +1,21 @@
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
+end
+
+vim.opt.rtp:prepend(lazypath)
+
 vim.o.number = true
 vim.o.wrap = false
 vim.o.tabstop = 2
@@ -7,8 +25,16 @@ vim.o.ignorecase = true
 vim.o.hlsearch = false
 vim.o.signcolumn = 'yes'
 
--- Keyboard Mappings
 vim.g.mapleader = vim.keycode('<Space>')
 
+-- Setup lazy.nvim
+require('lazy').setup({
+	spec = {},
+	install = {},
+	-- automatically check for plugin updates
+	checker = { enabled = true },
+})
+
+-- Keyboard Mappings
 vim.keymap.set({'n', 'x', 'o'}, 'gy', '"+y', {desc = 'Copy to clipboard'})
 vim.keymap.set({'n', 'x', 'o'}, 'gp', '"+p', {desc = 'Paste from clipboard'})
